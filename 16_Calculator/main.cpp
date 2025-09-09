1a8d7c7119ed74bf303fce2aa5ed8bddce41a619
Akasaka <yukiyou166@gmail.com>
2025-09-09 19:49:45 +0800

    create a simple calculator from the tutorial

---
+++ 16_Calculator/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    float a,b, hasil;
+    char aritmatika; 
+
+    cout << "selamat datang di program calculator \n\n"; 
+    
+    // masukan input dari
+    cout << "Masukan nilai pertama: ";
+    cin >> a;
+    cout << "Masukan operator +,-,/,*: ";
+    cin >> aritmatika;
+    cout << "Masukan nilai kedua: ";
+    cin >> b;
+
+    cout << "\nHasil perhitungan : ";
+    cout << a << aritmatika << b ;
+
+    if(aritmatika == '+')
+        hasil = a + b;
+    else if(aritmatika == '-')
+        hasil = a - b;
+    else if(aritmatika == '*')
+        hasil = a * b;
+    else if(aritmatika == '/')
+        hasil = a / b;
+    else 
+        cout << "operator anda salah!!" << endl;
+    
+    cout << " = " << hasil << endl;
+        
+        
+
+    return 0;
+}
